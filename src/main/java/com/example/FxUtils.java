c05f510013d6f5e92a4a852336d1096c28ed43ee
Andreas Kaiberger <andreas.kaiberger@gmail.com>
2025-11-14 20:34:43 +0100

    Changed FxUtils to final

--- src/main/java/com/example/FxUtils.java
+++ src/main/java/com/example/FxUtils.java
@@ -2,7 +2,7 @@ package com.example;
 
 import javafx.application.Platform;
 
-public class FxUtils {
+public final class FxUtils {
     /**
      * Runs task on the FX thread if possible, otherwise inline (e.g., in tests/headless mode)
      */
