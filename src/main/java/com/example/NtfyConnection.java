9f796f985eabf5988c70d8be000f85f1e43b3bca
Andreas Kaiberger <andreas.kaiberger@gmail.com>
2025-11-14 15:31:53 +0100

    Added javadoc in NtfyConnection
    Adjusted som ui design and css

--- src/main/java/com/example/NtfyConnection.java
+++ src/main/java/com/example/NtfyConnection.java
@@ -3,16 +3,31 @@ package com.example;
 import java.util.function.Consumer;
 
 public interface NtfyConnection {
-
+   /**
+     * Sends a message asynchronously.
+     *
+     * @param message the message to send
+     * @param callback invoked when send completes; receives true on success, false on failure.
+     *                 May be called on any thread.
+     */
     void send(String message, Consumer<Boolean> callback);
 
+
+    /**
+     * Registers a handler to receive incoming messages.
+     * Calling this multiple times replaces the previous handler.
+     *
+     * @param messageHandler invoked for each incoming message; may be called on any thread
+     */
     void receive(Consumer<NtfyMessageDto> messageHandler);
+
+    void setCurrentTopic(String topic);
+
+
 
     default String getCurrentTopic() {
         return "mytopic";
     }
-
-    void setCurrentTopic(String topic);
 
     default String getUserId() {
         return "unknown";
